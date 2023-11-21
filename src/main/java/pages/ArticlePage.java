46f5c017049843f375d45528527fbcdf4633d79f
Shariq <shariq.contour@gmail.com>
2023-11-21 18:26:40 +0500

    Article page 21-11-2023

---
+++ src/main/java/pages/ArticlePage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import com.microsoft.playwright.Page;
+
+public class ArticlePage {
+
+    private Page page;
+    public ArticlePage(Page page)
+    {
+        this.page = page;
+    }
+
+
+
+}
